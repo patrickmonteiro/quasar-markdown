5e06d5bd3cf05fc13d72fa6b6cf31c50e6beb67a
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2019-12-07 10:10:10 -0300

    First Commit :tada:

---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="container q-pa-md">
+    <q-markdown>
+    # Title
+    :::
+    Put your markdown here
+
+    Classic markup: :wink: :joy: :cry: :angel: :heart: :beers: :laughing: :yum:
+
+    Shortcuts (emoticons): :-) :-( 8-) ;)
+    :::
+    </q-markdown>
+    <q-markdown>
+    # Title
+      :::
+      Put your markdown here
+
+      Classic markup: :wink: :joy: :cry: :angel: :heart: :beers: :laughing: :yum:
+
+      Shortcuts (emoticons): :-) :-( 8-) ;)
+      :::
+    </q-markdown>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
